d0edf599d2ba52698a0d6dbe2ad854d8513b2ba0
TheSlavicFox <warren.troyer@outlook.com>
2022-09-08 12:18:39 -0400

    Added Vanilla version
    should work for NSB any version no major mods

---
+++ Vanilla/common/defines/Freediv_defines.lua
@@ -0,0 +1,3 @@
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0 -- Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 0 -- Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 0
---
+++ Vanilla/descriptor.mod
@@ -0,0 +1,6 @@
+version="Beta 1"
+tags={
+	"Utilities"
+}
+name="Free Divsion Editing"
+supported_version="1.11.*"
